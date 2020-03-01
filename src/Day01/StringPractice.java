971717073d019a7d60b6f87b22d10692e943c913
ayah.n <ayah.noor0213@gmail.com>
2020-03-01 02:48:36 -0500

    modified StringPractice class

---
+++ src/Day01/StringPractice.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class StringPractice {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        String name = "Ayah";
+        System.out.println(name);
+    }
+}
